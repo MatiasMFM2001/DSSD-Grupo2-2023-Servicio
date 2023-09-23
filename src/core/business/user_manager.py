b2493cb91586cbd1e9c9f94b7c9677661405f240
Matías Manzin <matias.manzin@outlook.com.ar>
2023-09-23 20:36:45 -0300

    Agregar `AuthManager.permission_required()`

--- src/core/business/user_manager.py
+++ src/core/business/user_manager.py
@@ -1,8 +1,11 @@
 from src.core.business.account_manager import AccountManager
 from src.core.database.auth import User
-from flask import session, abort
+from src.api.helpers.jwt_helpers import try_decode, encode
+from src.api.helpers.api_responses import SimpleErrorResponse
+from flask import request
 import functools
 import re
+from typing import Optional
 
 
 """Capa de negocio de alto nivel, para la tabla de Usuarios de la BD, donde se requiere hacer CRUDs sobre sus tuplas."""
@@ -127,6 +130,40 @@ class UserManager(AccountManager):
 
 """Capa de negocio de alto nivel, para la tabla de Usuarios de la BD, donde se requiere hacer CRUDs sobre sus tuplas, y manejar autenticación, sesiones y permisos."""
 class AuthManager(UserManager):
+    def token_of(self, associate) -> str:
+        return encode(id=associate.id)
+
+    def current_token(self) -> Optional[str]:
+        full_header = request.headers.get("Authorization", "")
+        header_args = full_header.split(" ")
+
+        if len(header_args) != 2:
+            return None
+
+        auth_type = header_args[0]
+        auth_credentials = header_args[1]
+
+        if auth_type != "Bearer":
+            return None
+
+        return auth_credentials
+
+    def current_id(self) -> Optional[int]:
+        token = self.current_token()
+
+        if token is None:
+            return None
+
+        return try_decode(token).get("id")
+
+    def current_user(self) -> Optional[Associate]:
+        associate_id = self.current_id()
+
+        if associate_id is None:
+            return None
+
+        return self.database.get(associate_id)
+    
     def has_permission(self, user_id, permit_name):
         """Verifica si un usuario tiene un permiso.
 
@@ -150,7 +187,56 @@ class AuthManager(UserManager):
         """
         return self.has_permission(self.current_id(), permit_name)
 
-    def permission_required(self, permit_name, login_required=True):
+    def is_current_logged_in(self) -> bool:
+        user_id = self.current_id()
+
+        if user_id is None:
+            return False
+
+        return self.database.exists(associate_id)
+
+    def login_required(self, call_with_current_associate=False):
+        """Verifica si el usuario está logueado.
+
+        Args:
+            call_with_current_associate (bool): Si es True, el primer argumento
+                que se le pasará a la función decorada será el socio actual (el
+                que llamó al endpoint).
+        """
+
+        def decorator(func):
+            """Recibe una función y la retorna con la validación de login.
+            Args:
+                func (function): Función a validar.
+
+            Returns:
+                any: Resultado de la función.
+            """
+
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                """Verifica si el usuario está logueado.
+
+                Returns:
+                    any: Resultado de la función.
+                """
+                if not self.is_current_logged_in():
+                    return SimpleErrorResponse(
+                        401, "El socio no tiene una sesión iniciada"
+                    )
+
+                if not call_with_current_associate:
+                    return func(*args, **kwargs)
+
+                return func(self.current_user(), *args, **kwargs)
+
+            return wrapper
+
+        return decorator
+    
+    # Fin nuevo
+    
+    def permission_required(self, permit_name, login_required=True, call_with_current_user=False):
         """Verifica si el usuario está logueado y tiene un permiso.
 
         Args:
@@ -174,13 +260,20 @@ class AuthManager(UserManager):
                 Returns:
                     any: Resultado de la función.
                 """
-                if login_required and not "id" in session:
-                    abort(401)
+                if login_required and not self.is_current_logged_in():
+                    return SimpleErrorResponse(
+                        401, "El usuario no tiene una sesión iniciada"
+                    )
 
                 if not self.has_permission(self.current_id(), permit_name):
-                    abort(403)
+                    return SimpleErrorResponse(
+                        403, f"El usuario no tiene el permiso {permit_name}"
+                    )
 
-                return func(*args, **kwargs)
+                if not call_with_current_user:
+                    return func(*args, **kwargs)
+
+                return func(self.current_user(), *args, **kwargs)
 
             return wrapper
 
