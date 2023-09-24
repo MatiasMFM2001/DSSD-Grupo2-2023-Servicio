a29eea81f7d08d7e221821ec91644bfd7b08a06f
Matías Manzin <matias.manzin@outlook.com.ar>
2023-09-24 02:38:54 -0300

    Eliminar archivo `club_api_bp`

--- src/api/blueprints/root_api_bp.py
+++ src/api/blueprints/root_api_bp.py
@@ -1,15 +1,13 @@
 from flask import Blueprint
 
 from src.api.blueprints.auth_api_bp import auth_api_bp
-from api.blueprints.user_api_bp import associates_api_bp
-from src.api.blueprints.club_api_bp import club_api_bp
+from src.api.blueprints.associates_api_bp import associates_api_bp
 
 root_api_bp = Blueprint("api", __name__, url_prefix="/api")
 
 for blueprint in [
     auth_api_bp,
     associates_api_bp,
-    club_api_bp,
 ]:
     root_api_bp.register_blueprint(blueprint)
 
