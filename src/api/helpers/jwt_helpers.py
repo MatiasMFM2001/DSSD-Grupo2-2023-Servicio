bfeeb3e94b1c2fd71406fc330101c5e4c2aecbd9
Matías Manzin <matias.manzin@outlook.com.ar>
2023-10-19 22:27:55 -0300

    Hacer que los tokens expiren tras 10 segundos

--- src/api/helpers/jwt_helpers.py
+++ src/api/helpers/jwt_helpers.py
@@ -2,16 +2,28 @@ from __future__ import annotations
 from flask import current_app
 import jwt
 from jwt.exceptions import InvalidTokenError
+from datetime import datetime, timezone, timedelta
 
 algorithm = "HS256"
 
 
 def encode(**values: any) -> str:
-    return jwt.encode(values, current_app.secret_key, algorithm=algorithm)
+    return jwt.encode(
+        {
+            "exp": datetime.now(tz=timezone.utc) + timedelta(seconds=10),
+            **values
+        },
+        current_app.secret_key,
+        algorithm=algorithm
+    )
 
 
 def decode(token: str) -> dict[str, any]:
-    return jwt.decode(token, current_app.secret_key, algorithms=[algorithm])
+    return jwt.decode(
+        token,
+        current_app.secret_key,
+        algorithms=[algorithm]
+    )
 
 
 def try_decode(token: str, default: dict[str, any] = {}) -> dict[str, any]:
