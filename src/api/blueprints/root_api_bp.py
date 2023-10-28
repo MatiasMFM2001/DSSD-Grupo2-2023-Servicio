0145cca3ff0eead5b3a1a1750fb65368ada8d171
Matías Manzin <matias.manzin@outlook.com.ar>
2023-10-28 18:21:55 -0300

    Eliminar `user_api_bp`

--- src/api/blueprints/root_api_bp.py
+++ src/api/blueprints/root_api_bp.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 
 from src.api.blueprints.auth_api_bp import auth_api_bp
-from src.api.blueprints.user_api_bp import user_api_bp
 from src.api.blueprints.material_api_bp import material_api_bp
 from src.api.blueprints.fabrication_slot_api_bp import fabrication_slot_api_bp
 from src.api.blueprints.material_requests_api_bp import material_requests_api_bp
@@ -11,7 +10,6 @@ root_api_bp = Blueprint("api", __name__, url_prefix="/api")
 
 for blueprint in [
     auth_api_bp,
-    user_api_bp,
     material_api_bp,
     fabrication_slot_api_bp,
     material_requests_api_bp,
