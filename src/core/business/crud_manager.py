c66042e94012bcf24dbc35d9bbc366f08b45a248
Matías Manzin <matias.manzin@outlook.com.ar>
2023-11-03 02:44:19 -0300

    Crear método `get_all()` en `CRUDManager`

--- src/core/business/crud_manager.py
+++ src/core/business/crud_manager.py
@@ -51,6 +51,9 @@ class CRUDManager(BusinessManager):
         """
         return self.database.get(id, include_inactive)
 
+    def get_all(self, id_list, include_inactive=False):
+        return [self.get(id, include_inactive) for id in id_list]
+    
     def update(self, id, include_inactives=False, **kwargs):
         """Actualiza una instancia en la base de datos con los valores especificados.
 
