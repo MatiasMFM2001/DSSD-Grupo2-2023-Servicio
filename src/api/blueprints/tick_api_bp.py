c9481f331bc633772cad69ae9cc12bb746d0c676
Matías Manzin <matias.manzin@outlook.com.ar>
2023-11-22 21:52:58 -0300

    Terminar implementacion de endpoint `tick_progreso()`

--- src/api/blueprints/tick_api_bp.py
+++ src/api/blueprints/tick_api_bp.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, request
+
+from src.core.business.fabrication_slot_manager import FabricationSlotManager
+
 from src.api.helpers import paginator_to_json, to_json
 from src.api.helpers.api_responses import SimpleOKResponse, SimpleErrorResponse
 from src.service.helpers.controller_helpers import get_int, api_validate_id
@@ -8,19 +11,25 @@ from werkzeug.exceptions import HTTPException
 from random import uniform
 
 tick_api_bp = Blueprint("tick_api_bp", __name__, url_prefix="/tick")
+fabrication_slots_m = FabricationSlotManager()
 
 @tick_api_bp.route("/percentage", methods=["GET"])
 @auth_m.permission_required("tick_show")
 def tick_progreso():
     """Obtiene porcentaje de progreso."""
     
-    percentage = round(uniform(0.0, 10.0), 2)
+    for slot in fabrication_slots_m.filter_by_get_list(reserved=True):
+        progress = slot.fabrication_progress + uniform(0.0, 10.0)
+        progress = min(progress, 100.0)
+        
+        fabrication_slots_m.update(slot.id, fabrication_progress=progress)
+    
     #slot, error = api_validate_id(fabrication_slots_m, request.args, tuple_name="El slot")
 
     #if error:
     #    return error
 
-    return SimpleOKResponse(percentage=percentage)
+    return SimpleOKResponse()
 
 @tick_api_bp.route("/arrival", methods=["GET"])
 @auth_m.permission_required("tick_show")
